8d81ad8f3daec3aee2ae290ea5734934645f43f7
Jamie Oaks <joaks1@gmail.com>
2013-04-22 00:20:28 -0500

    Adding `generateIntegerPartitions` function.
    Adding function to `partitionCombinatorics.c` and
    `partitionCombinatorics.h` that generates all partitions of an
    integer. The code is based on the ZS1 algorithm of Zoghbi
    and Stojmenovic (1998) Intern. J. Computer Math. 70:319--332.

--- src/partitionCombinatorics.h
+++ src/partitionCombinatorics.h
@@ -7,9 +7,10 @@ int integerPartitionCumulativeSpectrum(int n, int arr[]);
 int integerPartitionSpectrum(int n, int arr[]);
 double integerPartitionCumulativeProbs(int n, double arr[]);
 double integerPartitionProbs(int n, double arr[]);
-double integerPartition(int n);
+int integerPartition(int n);
 int drawIntegerPartitionCategory(gsl_rng * rng, int n);
 int dirichletProcessDraw(gsl_rng * rng, int n, double alpha, int elements[]);
+void generateIntegerPartitions(int n, int ip, int partitions[ip][n]);
 
 #endif
 
