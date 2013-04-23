75835217751849e927b77998b1ed7cf6ed34b5dc
Jamie Oaks <joaks1@gmail.com>
2013-04-22 21:05:42 -0500

    Updating `partitionCombinatorics`.
    Fixing return value error (`double` -> `int`) for `integerPartition`
    function in `partitionCombinatorics.c`.
    
    Changing the type of `partitions` argument for
    `generateIntegerPartitions` from 2-D array to an array of pointers.
    
    Adding `const` type for all `gsl_rng` arguments.

--- src/partitionCombinatorics.h
+++ src/partitionCombinatorics.h
@@ -1,5 +1,5 @@
-#ifndef INTEGER_PARTITION_H
-#define INTEGER_PARTITION_H
+#ifndef PARTITION_COMBINATORICS_H
+#define PARTITION_COMBINATORICS_H
 
 #include <gsl/gsl_rng.h>
 
@@ -8,9 +8,9 @@ int integerPartitionSpectrum(int n, int arr[]);
 double integerPartitionCumulativeProbs(int n, double arr[]);
 double integerPartitionProbs(int n, double arr[]);
 int integerPartition(int n);
-int drawIntegerPartitionCategory(gsl_rng * rng, int n);
-int dirichletProcessDraw(gsl_rng * rng, int n, double alpha, int elements[]);
-void generateIntegerPartitions(int n, int ip, int partitions[ip][n]);
+int drawIntegerPartitionCategory(const gsl_rng * rng, int n);
+int dirichletProcessDraw(const gsl_rng * rng, int n, double alpha, int elements[]);
+void generateIntegerPartitions(int n, int ip, int **partitions);
 
 #endif
 
