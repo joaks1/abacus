efe6fff35dbe11172076fcb68b6b923c1b94bc4f
Jamie Oaks <joaks1@gmail.com>
2013-05-16 22:20:43 -0500

    Adding more tests to `check_stats_utils.c`.

--- test/check_stats_utils.c
+++ test/check_stats_utils.c
@@ -1,10 +1,8 @@
 #include <stdlib.h>
 #include <check.h>
 #include <signal.h>
-#include "../src/array_utils.h"
-#include "../src/array_utils.c"
-#include "../src/stats_utils.h"
 #include "../src/stats_utils.c"
+#include "test_utils.h"
 
 START_TEST (test_init_free_sample_sum) {
     sample_sum * ss;
@@ -17,11 +15,26 @@ START_TEST (test_init_free_sample_sum) {
 }
 END_TEST
 
+START_TEST (test_update_sample_sum) {
+    double e = 0.000001;
+    sample_sum * ss;
+    ss = init_sample_sum();
+    update_sample_sum(ss, 1.0);
+    ck_assert_msg((ss->n == 1), "`sample_sum.n` is %d, expecting %d", ss->n, 1);
+    ck_assert_msg(almost_equal(ss->sum, 1.0, e), "`sample_sum.sum` is %lf, "
+            "expecting %lf", ss->sum, 1.0);
+    ck_assert_msg(almost_equal(ss->sum_of_squares, 1.0, e),
+            "`sample_sum.sum_of_squares is %lf, expecting %lf", ss->sum, 1.0);
+    free_sample_sum(ss);
+}
+END_TEST
+
 Suite * array_utils_suite(void) {
     Suite * s = suite_create("stats_utils");
 
     TCase * tc_sample_sum = tcase_create("sample_sum_test_case");
     tcase_add_test(tc_sample_sum, test_init_free_sample_sum);
+    tcase_add_test(tc_sample_sum, test_update_sample_sum);
     suite_add_tcase(s, tc_sample_sum);
 
     return s;
