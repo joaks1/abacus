876c1fc79139607661ff6307d873d8864ee61edc
Jamie Oaks <joaks1@gmail.com>
2013-08-09 18:03:40 -0500

    Adding more tests to `check_probability.c`.

--- test/check_probability.c
+++ test/check_probability.c
@@ -25,6 +25,90 @@ START_TEST (test_draw_gamma_or_uniform_gamma) {
 }
 END_TEST
 
+START_TEST (test_draw_gamma_or_uniform_zero) {
+    double e, shape, scale, d, sum, mn;
+    int i, reps;
+    gsl_rng * rng;
+    rng = get_rng(0);
+    reps = 100000;
+    shape = 0.0;
+    scale = 10.0;
+    for (i = 0; i < reps; i++) {
+        d = draw_gamma_or_uniform(rng, shape, scale);
+        ck_assert_msg((d > 0.0), "uniform draw %lf is less than zero", d);
+        sum += d;
+    }
+    e = 0.1;
+    mn = sum / reps;
+    ck_assert_msg(almost_equal(mn, 5.0, e), "mean of uniform draws "
+            "is %lf, expecting 5.0", mn);
+    free_rng(rng);
+}
+END_TEST
+
+START_TEST (test_draw_gamma_or_uniform_zero_neg) {
+    double e, shape, scale, d, sum, mn;
+    int i, reps;
+    gsl_rng * rng;
+    rng = get_rng(0);
+    reps = 100000;
+    shape = 0.0;
+    scale = -10.0;
+    for (i = 0; i < reps; i++) {
+        d = draw_gamma_or_uniform(rng, shape, scale);
+        ck_assert_msg((d > 0.0), "uniform draw %lf is less than zero", d);
+        sum += d;
+    }
+    e = 0.1;
+    mn = sum / reps;
+    ck_assert_msg(almost_equal(mn, 5.0, e), "mean of uniform draws "
+            "is %lf, expecting 5.0", mn);
+    free_rng(rng);
+}
+END_TEST
+
+START_TEST (test_draw_gamma_or_uniform_pos_neg) {
+    double e, shape, scale, d, sum, mn;
+    int i, reps;
+    gsl_rng * rng;
+    rng = get_rng(0);
+    reps = 100000;
+    shape = 20.0;
+    scale = -10.0;
+    for (i = 0; i < reps; i++) {
+        d = draw_gamma_or_uniform(rng, shape, scale);
+        ck_assert_msg((d > 0.0), "uniform draw %lf is less than zero", d);
+        sum += d;
+    }
+    e = 0.1;
+    mn = sum / reps;
+    ck_assert_msg(almost_equal(mn, 15.0, e), "mean of uniform draws "
+            "is %lf, expecting 15.0", mn);
+    free_rng(rng);
+}
+END_TEST
+
+START_TEST (test_draw_gamma_or_uniform_neg_neg) {
+    double e, shape, scale, d, sum, mn;
+    int i, reps;
+    gsl_rng * rng;
+    rng = get_rng(0);
+    reps = 100000;
+    shape = -2.0;
+    scale = -3.0;
+    for (i = 0; i < reps; i++) {
+        d = draw_gamma_or_uniform(rng, shape, scale);
+        ck_assert_msg((d > 0.0), "uniform draw %lf is less than zero", d);
+        sum += d;
+    }
+    e = 0.1;
+    mn = sum / reps;
+    ck_assert_msg(almost_equal(mn, 2.5, e), "mean of uniform draws "
+            "is %lf, expecting 2.5", mn);
+    free_rng(rng);
+}
+END_TEST
+
 
 Suite * probability_suite(void) {
     Suite * s = suite_create("probability");
@@ -32,6 +116,13 @@ Suite * probability_suite(void) {
     TCase * tc_draw_gamma_or_uniform = tcase_create(
             "draw_gamma_or_uniform_test_case");
     tcase_add_test(tc_draw_gamma_or_uniform, test_draw_gamma_or_uniform_gamma);
+    tcase_add_test(tc_draw_gamma_or_uniform, test_draw_gamma_or_uniform_zero);
+    tcase_add_test(tc_draw_gamma_or_uniform,
+            test_draw_gamma_or_uniform_zero_neg);
+    tcase_add_test(tc_draw_gamma_or_uniform,
+            test_draw_gamma_or_uniform_pos_neg);
+    tcase_add_test(tc_draw_gamma_or_uniform,
+            test_draw_gamma_or_uniform_neg_neg);
     suite_add_tcase(s, tc_draw_gamma_or_uniform);
 
     return s;
